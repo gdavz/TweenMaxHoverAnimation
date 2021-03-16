4d0a049c0afab2440b44352522999d808aafa7f5
gdavz <gdavz22@gmail.com>
2021-03-15 20:45:19 -0400

    Create hover-images.js

---
+++ js/hover-images.js
@@ -0,0 +1,104 @@
+/******* Here are the hover images enjoy *******
+Simple Custom CSS and JS - by theclearstudio.com
+*/
+new hoverEffect({
+parent: document.querySelector('.distort'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/AIfbUyzhKz0',
+image2: 'https://source.unsplash.com/AIfbUyzhKz0',
+displacementImage: '/im/d1.jpg'
+});
+
+new hoverEffect({
+parent: document.querySelector('.distort2'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/3rqYH_y9M2M',
+image2: 'https://source.unsplash.com/3rqYH_y9M2M',
+displacementImage: '/im/d2.jpg'
+});
+
+new hoverEffect({
+parent: document.querySelector('.distort3'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/sotrQtj-fF0',
+image2: 'https://source.unsplash.com/sotrQtj-fF0',
+displacementImage: '/im/d3.jpg'
+});
+
+new hoverEffect({
+parent: document.querySelector('.distort4'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/yKxJeQk29zI',
+image2: 'https://source.unsplash.com/yKxJeQk29zI',
+angel: Math.PI / 4,
+displacementImage: '/im/d4.jpg'
+});
+
+new hoverEffect({
+parent: document.querySelector('.distort5'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/WMryoUiJonk',
+image2: 'https://source.unsplash.com/WMryoUiJonk',
+displacementImage: '/im/d5.jpg'
+});
+
+new hoverEffect({
+parent: document.querySelector('.distort6'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/lMEbEu1FIIQ',
+image2: 'https://source.unsplash.com/lMEbEu1FIIQ',
+displacementImage: '/im/d6.jpg'
+});
+
+
+new hoverEffect({
+parent: document.querySelector('.distort7'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/TkLQjXIIP20',
+image2: 'https://source.unsplash.com/TkLQjXIIP20',
+displacementImage: '/im/d7.jpg'
+});
+
+
+new hoverEffect({
+parent: document.querySelector('.distort8'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/UZ2gS0NaFGE',
+image2: 'https://source.unsplash.com/UZ2gS0NaFGE',
+displacementImage: '/im/d8.jpg'
+});
+
+
+new hoverEffect({
+parent: document.querySelector('.distort9'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/1qCSGzVEKKQ',
+image2: 'https://source.unsplash.com/1qCSGzVEKKQ',
+displacementImage: '/im/d9.jpg'
+});
+
+new hoverEffect({
+parent: document.querySelector('.distort10'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/mCqZOhV8-kU',
+image2: 'https://source.unsplash.com/mCqZOhV8-kU',
+displacementImage: '/im/d10.jpg'
+});
+
+
+new hoverEffect({
+parent: document.querySelector('.distort11'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/yw9z-otDqMw',
+image2: 'https://source.unsplash.com/yw9z-otDqMw',
+displacementImage: '/im/d11.jpg'
+});
+
+
+new hoverEffect({
+parent: document.querySelector('.distort12'),
+intensity: 0.2,
+image1: 'https://source.unsplash.com/jIJuwlQHou8',
+image2: 'https://source.unsplash.com/jIJuwlQHou8',
+displacementImage: '/im/d12.jpg'
+});
